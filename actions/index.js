c6436d18c512e385732d9e202e83005d005b706d
RealSadLemon <esmodearburk@gmail.com>
2022-06-15 23:05:37 -0500

    Composed action creator functions in actions/index.js

--- actions/index.js
+++ actions/index.js
@@ -3,3 +3,29 @@ export const GET_ACTIVITY_OF_TYPE = 'GET_ACTIVITY_OF_TYPE';
 export const GET_ACTIVITY_OF_PRICE = 'GET_ACTIVITY_OF_PRICE';
 export const GET_ACTIVITY_OF_PARTICIPANTS = 'GET_ACTIVITY_OF_PARTICIPANTS';
 
+export const getActivityObject = () => {
+    return {
+        type: GET_ACTIVITY_OBJECT
+    }
+}
+
+export const getActivityOfType = (type) => {
+    return {
+        type: GET_ACTIVITY_OF_TYPE,
+        payload: type
+    }
+}
+
+export const getActivityOfPrice = (price) => {
+    return {
+        type: GET_ACTIVITY_OF_TYPE,
+        payload: price
+    }
+}
+
+export const getActivityOfParticipants = (numOfParticipants) => {
+    return {
+        type: GET_ACTIVITY_OF_PARTICIPANTS,
+        payload: numOfParticipants
+    }
+}
